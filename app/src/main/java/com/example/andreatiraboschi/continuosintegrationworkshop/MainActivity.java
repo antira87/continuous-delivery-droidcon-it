218036d022b197742127a91897762d5a9d8c70d7
andreatiraboschi <antira87@gmail.com>
2017-04-06 14:38:46 +0200

    crashlytics

--- app/src/main/java/com/example/andreatiraboschi/continuosintegrationworkshop/MainActivity.java
+++ app/src/main/java/com/example/andreatiraboschi/continuosintegrationworkshop/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.andreatiraboschi.continuosintegrationworkshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.ndk.CrashlyticsNdk;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         setContentView(R.layout.activity_main);
     }
 }
